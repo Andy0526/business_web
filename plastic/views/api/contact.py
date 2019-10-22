6c51cd717e8b8cb5156d99b4a332bd0d9b656de2
andy <liusan0526@gmail.com>
2019-10-22 20:16:31 +0800

    fix:fix contact url

--- plastic/views/api/contact.py
+++ plastic/views/api/contact.py
@@ -5,6 +5,6 @@ from flask import Blueprint, render_template
 bp = Blueprint('contact', __name__, url_prefix='')
 
 
-@bp.route('/contanct', methods=['GET'])
+@bp.route('/contact', methods=['GET'])
 def get_news():
     return render_template('contact.html')
