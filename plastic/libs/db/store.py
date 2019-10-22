480fb3bd56fa4a4608d1be486b89d74caf8c2c8a
andy <liusan0526@gmail.com>
2019-10-22 20:12:41 +0800

    feat: rm db init

--- plastic/libs/db/store.py
+++ plastic/libs/db/store.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
+#
+# from plastic.config import SQLALCHEMY_DATABASE_URI
 
-from plastic.config import SQLALCHEMY_DATABASE_URI
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URI, max_overflow=80, pool_size=100, pool_recycle=3600,
-    isolation_level="AUTOCOMMIT"
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URI, max_overflow=80, pool_size=100, pool_recycle=3600,
+#     isolation_level="AUTOCOMMIT"
+# )
 # db = engine.connect()
 db = None
