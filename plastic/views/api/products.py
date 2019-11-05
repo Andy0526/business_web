649d931d9ddecde8d6a878ee3468e42e6883203d
123 <123@123.com>
2019-11-05 22:13:46 +0800

    Update: update prod pages

--- plastic/views/api/products.py
+++ plastic/views/api/products.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('products', __name__, url_prefix='')
 
-
+@bp.route('/products', methods=['GET'])
 @bp.route('/products/<int:item_id>', methods=['GET'])
-def get_products(item_id=0):
-    return render_template('products.html')
+def get_products(item_id=1):
+    return render_template('products.html',item_id=item_id)
