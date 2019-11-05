75e147f2f4ccfa2e4a25b204e84d42c1114d97aa
andy <liusan0526@gmail.com>
2019-11-05 21:02:44 +0800

    feat:add products url

--- plastic/views/api/products.py
+++ plastic/views/api/products.py
@@ -5,6 +5,6 @@ from flask import Blueprint, render_template
 bp = Blueprint('products', __name__, url_prefix='')
 
 
-@bp.route('/products', methods=['GET'])
-def get_products():
+@bp.route('/products/<int:item_id>', methods=['GET'])
+def get_products(item_id=0):
     return render_template('products.html')
